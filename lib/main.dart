7ceb17688819d2d0f8e36c1e311148f1a7a31ce7
blrzzzt <5607000+blrzzzt@users.noreply.github.com>
2023-04-21 03:17:07 -0400

    good enough

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_chat_bubble/chat_bubble.dart';
+import 'package:flutter_chat_bubble/bubble_type.dart';
+import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_7.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'True Match',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,22 +28,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'You Truly Matched!'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,17 +43,86 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<String> messageList = [
+    'hey what\'s up cutie? glad we matched',
+    'really that is so cool i thought so too!',
+    'honestly i never thought about using this app it just was kind of a spur of the moment thing.. :p'
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  List<Widget> currentMessageList = [];
+
+  late TextEditingController _controller;
+  int messageIndex = 2;
+  bool receive = true;
+
+  Widget genNewBubble(String message) {
+    List<Widget> bubbleRow = [
+      Padding(
+        padding:
+            receive ? EdgeInsets.only(right: 12) : EdgeInsets.only(left: 12),
+        child: Container(
+          clipBehavior: Clip.antiAlias,
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.all(Radius.circular(25))),
+          child: receive
+              ? Image.asset(
+                  'assets/matcher.jpg',
+                  fit: BoxFit.cover,
+                )
+              : Image.asset('assets/me.jpg', fit: BoxFit.cover),
+        ),
+      ),
+      Expanded(
+        child: ChatBubble(
+          alignment: receive ? Alignment.centerLeft : Alignment.centerRight,
+          backGroundColor: receive
+              ? const Color.fromRGBO(105, 255, 250, 1)
+              : const Color.fromRGBO(255, 45, 140, 1),
+          clipper: ChatBubbleClipper6(
+              type:
+                  receive ? BubbleType.receiverBubble : BubbleType.sendBubble),
+          child: Text(
+            message,
+            style: receive
+                ? TextStyle(color: Colors.black)
+                : TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Row(children: receive ? bubbleRow : [bubbleRow[1], bubbleRow[0]]),
+    );
+  }
+
+  void newMessage() async {
+    currentMessageList.add(genNewBubble(messageList[messageIndex]));
+    await Future.delayed(const Duration(seconds: 1)).then((value) {
+      setState(() {
+        currentMessageList;
+        receive = !receive;
+      });
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      newMessage();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -70,46 +134,125 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: PreferredSize(
+        preferredSize: Size(MediaQuery.of(context).size.width, 80),
+        child: Container(
+          decoration: BoxDecoration(color: Color.fromRGBO(60, 135, 255, 0.2)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Icon(Icons.arrow_back_ios_new_rounded),
+              ),
+              Expanded(
+                  child: FittedBox(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+                  child: Text(
+                    'Chat Up Your Match!',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                ),
+              )),
+              Expanded(
+                child: Image.asset(
+                  'trueMatch.png',
+                  fit: BoxFit.fitHeight,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      resizeToAvoidBottomInset: true,
+      body: Container(
+        decoration:
+            BoxDecoration(color: Colors.lightBlueAccent.withOpacity(0.1)),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SingleChildScrollView(
+              reverse: true,
+              child: Column(
+                children: [
+                  Container(
+                    child: Column(
+                      children: currentMessageList,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: Color.fromRGBO(255, 60, 90, 0.5)),
+                      height: 0.4,
+                    ),
+                  ),
+                  Container(
+                    // decoration: BoxDecoration(color: Colors.black12),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 8.0, right: 8, bottom: 16),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              cursorColor: Color.fromRGBO(255, 60, 90, 0.2),
+                              cursorRadius: Radius.circular(25),
+                              cursorHeight: 25,
+                              cursorWidth: 2,
+                              controller: _controller,
+                              maxLines: null,
+                              textAlign: TextAlign.center,
+                              decoration: InputDecoration(
+                                suffixIconColor:
+                                    Color.fromRGBO(255, 60, 90, 0.5),
+                                suffixIcon: IconButton(
+                                    focusColor:
+                                        Color.fromRGBO(255, 60, 90, 0.5),
+                                    splashRadius: 25,
+                                    splashColor:
+                                        Color.fromRGBO(255, 60, 90, 0.2),
+                                    onPressed: () {
+                                      if (_controller.text.length > 0) {
+                                        currentMessageList.add(
+                                            genNewBubble(_controller.text));
+                                        _controller.text = '';
+                                        _controller.clearComposing();
+                                        setState(() {
+                                          currentMessageList;
+                                          receive = !receive;
+                                        });
+                                      }
+                                    },
+                                    icon: Icon(Icons.send_sharp)),
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: const Color.fromRGBO(
+                                            255, 60, 90, 0.5))),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color:
+                                            Color.fromRGBO(255, 60, 90, 0.2))),
+                                labelStyle: TextStyle(
+                                    color: Color.fromRGBO(255, 60, 90, 0.5)),
+                                floatingLabelStyle: TextStyle(
+                                    color: Color.fromRGBO(255, 60, 90, 0.2)),
+                                labelText: 'Say Something True!',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
